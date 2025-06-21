01b935138e11e2b7a8725de9fcb89844e8fc5ce8
Abinayanafaiq <abinayanafaiq550@gmail.com>
2025-06-21 10:47:09 +0700

    TodoList mongoDB next js

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,20 +1,55 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
 
-const Navbar = () => {
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="flex flex-row gap-4 p-4 bg-gray-100 shadow">
-      <Link className="hover:underline" href={"/"}>
-        Home
-      </Link>
-      <Link className="hover:underline" href={"/my-task"}>
-        Mytask
-      </Link>
-      <Link className="hover:underline" href={"/add-task"}>
-        AddTask
-      </Link>
-    </div>
-  );
-};
+    <nav className="bg-gray-100 shadow-md">
+      <div className="max-w-6xl mx-auto px-4">
+        <div className="flex justify-between items-center h-16">
+          <div className="text-xl font-bold">ToDoApp</div>
+          <div className="md:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className="text-gray-700 focus:outline-none"
+            >
+              {isOpen ? <X size={24} /> : <Menu size={24} />}
+            </button>
+          </div>
+          {/* Menu Links -Dekstop */}
+          <div className="hidden md:flex gap-6 items-center">
+            <Link href={"/"} className="hover:underline">
+              Home
+            </Link>
+            <Link href={"my-task"} className="hover:underline">
+              My Task
+            </Link>
+            <Link href={"/add-task"} className="hover:underline">
+              Add Task
+            </Link>
+          </div>
+        </div>
+      </div>
 
-export default Navbar;
+      {/* Menu Links - Mobile */}
+      {isOpen && (
+        <div className="md:hidden px-4 pb-4 flex flex-col gap-3">
+          <Link href={"/"} onClick={() => setIsOpen(false)}>
+            Home
+          </Link>
+          <Link href={"/my-task"} onClick={() => setIsOpen(false)}>
+            My Task
+          </Link>
+          <Link href={"/add-task"} onClick={() => setIsOpen(false)}>
+            Add Task
+          </Link>
+        </div>
+      )}
+    </nav>
+  );
+}
--- components/TodoForm.tsx
+++ components/TodoForm.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 export default function TodoForm() {
   const [judul, setJudul] = useState("");
   const [deskripsi, setDeskripsi] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -17,6 +18,8 @@ export default function TodoForm() {
       body: JSON.stringify({ judul, deskripsi }),
     });
 
+    setLoading(false);
+
     if (ress.ok) {
       setJudul("");
       setDeskripsi("");
@@ -27,15 +30,22 @@ export default function TodoForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="space-y-4">
+    <form
+      onSubmit={handleSubmit}
+      className="space-y-6 bg-white p-6 rounded shadow-md"
+    >
+      <h2 className="text-xl font-bold text-gray-800">Tambah Tugas Baru</h2>
       <div>
-        <label className="block font-semibold">Judul Tugas: </label>
+        <label className="block font-medium text-gray-700 mb-1">
+          Judul Tugas:{" "}
+        </label>
         <input
           required
           type="text"
           value={judul}
           onChange={(e) => setJudul(e.target.value)}
-          className="border px-3 py-1 rounded w-full"
+          className="border border-gray-300 px-3 py-2 rounded w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+          placeholder="Contoh: Belajar NextJS"
         />
       </div>
       <div>
@@ -44,15 +54,21 @@ export default function TodoForm() {
           required
           value={deskripsi}
           onChange={(e) => setDeskripsi(e.target.value)}
-          className="border px-3 py-1 rounded w-full"
+          className="border border-gray-300 px-3 py-2 rounded w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+          placeholder="Contoh Buat Form tambah todo dan simpan ke mongoDB"
         />
       </div>
 
       <button
         type="submit"
-        className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
+        disabled={loading}
+        className={`w-full py-2 px-4 rounded text-white font-semibold ${
+          loading
+            ? "bg-gray-400 cursor-not-allowed"
+            : "bg-blue-600 hover:bg-blue-700"
+        }`}
       >
-        Tambahkan Tugas
+        {loading ? "Mengirim.." : "Tambahkan Tugas"}
       </button>
     </form>
   );
